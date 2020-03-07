054604c36dfb2a6ae0f567e6946c04b44366620f
Larko_0 <larionov1001@gmail.com>
2020-03-07 18:58:18 +0200

    hero + scills protortypes

--- Gameobjcets/subModels/hero.py
+++ Gameobjcets/subModels/hero.py
@@ -1,26 +1,44 @@
 from .scills import *
 
 
-class HeroPrototype(models.Model):
-    pass
-
-
-class Hero(models.Model):
-    pass
-
-
-class Stats(models.Model):
-    pass
-
-
 class AbstractBubble(models.Model):
+    vaule = models.IntegerField(default=0)
     class Meta:
         abstract = True
 
 
-class StatBubble(AbstractBubble):
-    pass
-
-
 class PriceBubble(AbstractBubble):
     pass
+
+
+class StatBubble(AbstractBubble):
+    next = models.OneToOneField(PriceBubble, blank=True, null=True, on_delete=models.SET_NULL,
+                                related_name="prev_bubble")
+    prev = models.OneToOneField(PriceBubble, blank=True, null=True, on_delete=models.SET_NULL,
+                                related_name='next_bubble')
+
+
+class Stats(models.Model):
+    attack = models.ForeignKey(StatBubble, on_delete=models.SET_NULL, blank=True, null=True,
+                               related_name="attack_stats")
+    magic = models.ForeignKey(StatBubble, on_delete=models.SET_NULL, blank=True, null=True,
+                              related_name="magic_stats")
+    armor = models.ForeignKey(StatBubble, on_delete=models.SET_NULL, blank=True, null=True,
+                              related_name="armor_stats")
+    range = models.ForeignKey(StatBubble, on_delete=models.SET_NULL, blank=True, null=True,
+                              related_name="range_stats")
+    speed = models.ForeignKey(StatBubble, on_delete=models.SET_NULL, blank=True, null=True,
+                              related_name="speed_stats")
+
+
+class HeroPrototype(models.Model):
+    stats = models.ForeignKey(Stats, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, default='', unique=True)
+    q = models.OneToOneField(Skill, on_delete=models.SET_NULL, blank=True, null=True, related_name='hero_q')
+    w = models.OneToOneField(Skill, on_delete=models.SET_NULL, blank=True, null=True, related_name='hero_w')
+    e = models.OneToOneField(Skill, on_delete=models.SET_NULL, blank=True, null=True, related_name='hero_e')
+    r = models.OneToOneField(Skill, on_delete=models.SET_NULL, blank=True, null=True, related_name='hero_r')
+
+
+class Hero(models.Model):
+    stats = models.ForeignKey(Stats, on_delete=models.CASCADE)
--- Gameobjcets/subModels/scills.py
+++ Gameobjcets/subModels/scills.py
@@ -2,7 +2,8 @@ from .effects import *
 
 
 class Skill(models.Model):
-    pass
+    name = models.CharField(max_length=40, default="")
+    cooldown = models.OneToOneField(ValuesOnLevels, on_delete=models.CASCADE)
 
 
 class SubSkill(models.Model):
@@ -11,12 +12,17 @@ class SubSkill(models.Model):
 
 
 class EffectsToTargetSubSkill(SubSkill):
-    pass
+    range = models.OneToOneField(ValuesOnLevels, on_delete=models.CASCADE)
+    effects = models.OneToOneField(EffectPrototype, on_delete=models.CASCADE)
+    damage = models.OneToOneField(Damage, on_delete=models.CASCADE)
 
 
 class EffectsInZoneSubSkill(SubSkill):
-    pass
+    effects = models.OneToOneField(EffectPrototype, on_delete=models.CASCADE)
+    damage = models.OneToOneField(Damage, on_delete=models.CASCADE)
 
 
 class EffectsInRadiusSubSkill(SubSkill):
-    pass
+    range = models.OneToOneField(ValuesOnLevels, on_delete=models.CASCADE)
+    effects = models.OneToOneField(EffectPrototype, on_delete=models.CASCADE)
+    damage = models.OneToOneField(Damage, on_delete=models.CASCADE)
